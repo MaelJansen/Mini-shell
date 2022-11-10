ba2d2e669043b3096e2f8400230ae7e314705b1c
Mael Jansen <maeljansen2003@gmail.com>
2022-11-10 17:06:43 +0100

    Reglages des problèmes de mémoire

--- minishell/Shell.c
+++ minishell/Shell.c
@@ -54,10 +54,18 @@ shell_read_line( struct Shell *this )
     getline( &this->line, &this->line_length, stdin );
 }
 
+/*
+* Fonction : do_help
+* ------------------
+* Affiche une aide pour savoir quelles commandes sont implémentées 
+*
+* *this : le shell actif 
+* *args : ce que l'utilisateur a rentrer dans le terminal
+*/
 static void
 do_help( struct Shell *this, const struct StringVector *args )
 {
-    printf( "-> commands: exit, cd, help, ?, pwd, external commands : ! commands -options.\n" );
+    printf( "-> commands: exit, cd, help, ?, pwd, jobs / external commands : ! commands -options.\n" );
     (void)this;
     (void)args;
 }
@@ -66,35 +74,40 @@ static void
 externalCMD(struct Shell *this, const struct StringVector *args) {
     char *tmp = malloc(256*sizeof(char));
     char *path = malloc(256*sizeof(char));
-    //char *options = malloc(256*sizeof(char));
     int j = 0;
         strjoinarray(tmp, args, 1, args->size, " ");
-        //strjoinarray(options, args, 1, args->size, " ");
-        //printf("test otions split : %s \n", options);
-        //char *tmp2[256] = {options, NULL};
-        char *tmp2[256];
+        char *tmp2[args->size];
         for (size_t i=1; i<args->size; i++){
-            tmp2[j] = args->strings[i];
-            j ++;
+            if (strcmp(args->strings[i],"&") != 0){
+                tmp2[j] = args->strings[i];
+                j++;
+            }
         }
-        tmp2[j+1] = NULL;
-        tmp2[j+2] = '\0';
+        tmp2[j] = '\0';
         strcat(path, "/bin/");
         strcat(path, args->strings[1]);
         execv(path, tmp2);
 
     free(path);
     free(tmp);
-    //free(options);
     (void)this;
 }
 
+/*
+* Fonction : do_system
+* --------------------
+* Permet de lancer les commandes externes correctement
+*   (en arrière plan si un '&' a été ajouté a la fin de la commande) 
+*
+* *this : le shell actif 
+* *args : ce que l'utilisateur a rentrer dans le terminal
+*/
 static void
 do_system( struct Shell *this, const struct StringVector *args )
 {
     pid_t p = fork();
     int s;
-    // Il fuat trouver un moyen pour supprimer les processus qui sont terminés
+    // Il faut trouver un moyen pour supprimer les processus qui sont terminés
     jobsTab[increment] = p;
     increment += 1;
         if (p == -1){
@@ -104,11 +117,20 @@ do_system( struct Shell *this, const struct StringVector *args )
             externalCMD(this, args);
             exit(EXIT_SUCCESS);
         }
-    if (strcmp(args->strings[args->size - 1],"&") != 0) {
+    if (strcmp(args->strings[args->size - 1],"&") != 0) {  
         waitpid(p, &s, 0);
     }
 }
 
+/*
+* Fonction : do_cd
+* ----------------
+* Permet d'effectuer la commande cd qui permet
+*   de changer de dossier
+*
+* *this : le shell actif 
+* *args : ce que l'utilisateur a rentrer dans le terminal
+*/
 static void
 do_cd( struct Shell *this, const struct StringVector *args )
 {
@@ -148,6 +170,15 @@ do_exit( struct Shell *this, const struct StringVector *args )
     (void)args;
 }
 
+/*
+* Fonction : do_pwd
+* -----------------
+* Permet de lancer la commande pwd qui permet
+*   de voir le chemin actuel
+*
+* *this : le shell actif 
+* *args : ce que l'utilisateur a rentrer dans le terminal
+*/
 static void
 do_pwd( struct Shell *this, const struct StringVector *args)
 {
@@ -162,6 +193,14 @@ do_pwd( struct Shell *this, const struct StringVector *args)
     (void)this;
 }
 
+/*
+* Fonction : do_jobs
+* ------------------
+* Permet de lancer la commande jobs qui affiche
+*   les processus en cours
+* *this : le shell actif 
+* *args : ce que l'utilisateur a rentrer dans le terminal
+*/
 static void
 do_jobs()
 {
@@ -172,6 +211,11 @@ do_jobs()
 
 typedef void ( *Action )( struct Shell *, const struct StringVector * );
 
+/*
+* Struct Action
+* ------------------
+* Regroupe toutes les actions (commandes) utilisables
+*/
 static struct {
     const char *name;
     Action      action;
